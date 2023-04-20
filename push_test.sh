a2e2eeee24cff71075fa3e3f7955ca9a8d150e2d
longzhili <longzhili2005@126.com>
2023-04-20 17:56:18 +0800

    fix v0.1.6

--- push_test.sh
+++ push_test.sh
@@ -1,4 +1,4 @@
-VERSION="v0.1.5"
+VERSION="v0.1.6"
 NAME="HSXCAPKit-3rdparty-libs"
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 git add .
